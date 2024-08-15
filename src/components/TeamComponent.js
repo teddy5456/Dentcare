79ebcb235138a9bfc3db39866ad964c2216f0a01
teddy5456 <teddywambua5456@gmail.com>
2024-08-15 10:18:54 +0300

    Team component

--- src/components/TeamComponent.js
+++ src/components/TeamComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './TeamComponent.css';
+import Emma from '../assets/emma.png'
 
 const teamMembers = [
   {
     name: 'Dr. Emma Wainaina',
     role: 'Practicing Orthodontic',
-    image: 'https://media.licdn.com/dms/image/C4E16AQE8mtisCmJvXg/profile-displaybackgroundimage-shrink_200_800/0/1639124044506?e=2147483647&v=beta&t=vmp4FIic1z_3AJFEyRFKcAvjdzVwLlBuijdHbJ_fJzY', // Replace with actual image URL
+    image: Emma,
     bio: `Dr. Emma is a Certified Dentist, Orthodontist, and pediatrician with over a decade of experience in smile resolution and teeth alignment. 
           She is the Founder & Director of Dentplan Dental Surgeons, where she offers day-to-day dental health services and manages the strategic development and growth of the firm.`,
     social: {
@@ -19,7 +20,7 @@ const teamMembers = [
   {
     name: 'Dr. Mwinzi',
     role: 'Certified Dentist and Implantologist',
-    image: 'https://via.placeholder.com/150', // Replace with actual image URL
+    image: 'https://ideogram.ai/assets/progressive-image/balanced/response/9fsiTkP-Q4i-v0vB6OnXbg',
     bio: `Dr. Mwinzi is a Certified Dentist and Implantologist with over 15 years of clinical practice experience. He is passionate about putting smiles on people's faces and is particularly keen on creating a younger generation that is more health-conscious. He aims to demystify dental health treatment as painful, expensive, and for the chosen few with Insurance.`,
     social: {
       twitter: '#',
@@ -27,14 +28,13 @@ const teamMembers = [
       github: '#',
     },
   },
-  // Add more team members here if needed
 ];
 
 const TeamComponent = () => {
   return (
     <div className="team-container">
       {teamMembers.map((member, index) => (
-        <div key={index} className="team-card">
+        <div key={index} className={`team-card ${index % 2 === 0 ? 'left-image' : 'right-image'}`}>
           <img src={member.image} alt={member.name} className="team-image" />
           <div className="team-info">
             <h3 className="team-name">{member.name}</h3>
